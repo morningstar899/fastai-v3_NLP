c7cff1b48787f10a401cd04908e5816b01c92765
Toronto899 <cai_anthony@yahoo.com>
2019-07-20 08:25:35 -0400

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,8 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+#export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+#export_file_name = 'export.pkl'
+export_file_url = 'https://drive.google.com/file/d/1VKGNI916BO6u_YCSSSbs8ey-8yUS8cK8/view?usp=sharing'
+export_file_name = 'fine_tuned.pkl'
 
 classes = ['black', 'grizzly', 'teddys']
 path = Path(__file__).parent
