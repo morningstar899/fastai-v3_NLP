53367078a3d649ac540b9ead00e3800425698187
Toronto899 <cai_anthony@yahoo.com>
2019-07-21 18:12:30 -0400

    Update client.js

--- app/static/client.js
+++ app/static/client.js
@@ -15,24 +15,25 @@ function showPicked(input) {
 }
 
 function analyze() {
-  var uploadFiles = el("file-input").files;
-  if (uploadFiles.length !== 1) alert("Please select a file to analyze!");
+    var uploadFiles = el('reviewText');
+    // var uploadFiles = el('file-input').files;
 
-  el("analyze-button").innerHTML = "Analyzing...";
-  var xhr = new XMLHttpRequest();
-  var loc = window.location;
-  xhr.open("POST", `${loc.protocol}//${loc.hostname}:${loc.port}/analyze`,
-    true);
-  xhr.onerror = function() {
-    alert(xhr.responseText);
-  };
-  xhr.onload = function(e) {
-    if (this.readyState === 4) {
-      var response = JSON.parse(e.target.responseText);
-      el("result-label").innerHTML = `Result = ${response["result"]}`;
+    // if (uploadFiles.length != 1) alert('Please select 1 file to analyze!');
+
+    el('analyze-button').innerHTML = 'Analyzing...';
+    var xhr = new XMLHttpRequest();
+    var loc = window.location
+    xhr.open('POST', `${loc.protocol}//${loc.hostname}:${loc.port}/analyze`, true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.onerror = function() {alert (xhr.responseText);}
+    xhr.onload = function(e) {
+        if (this.readyState === 4) {
+            console.log("e.target:", e.target)
+            var response = JSON.parse(e.target.responseText);
+            el('result-label').innerHTML = `Result = ${response['result']}`;
+        }
+        el('analyze-button').innerHTML = 'Analyze';
     }
-    el("analyze-button").innerHTML = "Analyze";
-  };
 
     var fileData = {
         "textField": uploadFiles.value
