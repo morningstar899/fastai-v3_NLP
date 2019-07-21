1379ab108ace62f35e0839c4640e62efa842525d
Toronto899 <cai_anthony@yahoo.com>
2019-07-21 18:14:26 -0400

    Update client.js

--- app/static/client.js
+++ app/static/client.js
@@ -5,13 +5,15 @@ function showPicker() {
 }
 
 function showPicked(input) {
-  el("upload-label").innerHTML = input.files[0].name;
-  var reader = new FileReader();
-  reader.onload = function(e) {
-    el("image-picked").src = e.target.result;
-    el("image-picked").className = "";
-  };
-  reader.readAsDataURL(input.files[0]);
+    // el('upload-label').innerHTML = input.files[0].name;
+    el('upload-label').innerHTML = input.name;
+    var reader = new FileReader();
+    reader.onload = function (e) {
+        el('image-picked').src = e.target.result;
+        el('image-picked').className = '';
+    }
+    // reader.readAsDataURL(input.files[0]);
+    reader.readAsText(input);
 }
 
 function analyze() {
