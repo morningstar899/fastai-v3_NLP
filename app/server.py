077cab91669f6b6d523238bcc39af6cc3e1a2959
Toronto899 <cai_anthony@yahoo.com>
2019-07-20 21:56:51 -0400

    Update server.py

--- app/server.py
+++ app/server.py
@@ -71,7 +71,8 @@ async def analyze(request):
     data = await request.form()
     content = data['content']
     prediction = learn.predict(content)[0]
-    return JSONResponse({'Review Rating': str(prediction)})
+#    return JSONResponse({'Review Rating': str(prediction)})
+    return JSONResponse({'result': str(prediction)})
 
 if __name__ == '__main__':
     if 'serve' in sys.argv:
